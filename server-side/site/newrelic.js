9643a816214acb198fe7876db9c38ca6fb91c281
Ubuntu <ubuntu@ip-172-31-68-105.ec2.internal>
2017-04-19 17:07:50 +0000

    Adding newrelic config files

---
+++ server-side/site/newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Checkbox-AWS'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'fd29a189190da75c2ae26d6755422cea00e0a6a4',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
